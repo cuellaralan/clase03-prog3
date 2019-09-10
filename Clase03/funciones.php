34a29cd2d8f9c55e5848bc7dc20cac51d8599259
Alan Cuellar <alan.javier.ajc@gmail.com>
2019-09-10 18:59:15 -0300

    Funciones
    Funcion modificar/listar/guardar

--- Clase03/funciones.php
+++ Clase03/funciones.php
@@ -25,6 +25,21 @@ class Funciones
     public static function ModificarxID($id,$objeto,$archivo)
     {   
         $array1 = funciones::Listar($archivo);
+        $array1[$id] = $objeto;
+        $apertura = "a";
+        unlink($archivo);
+        foreach($array1 as $unobjeto)
+        {
+            if(is_null($unobjeto))
+            {
+                break;
+            }
+            else
+            {
+                Funciones::Guardar($unobjeto,$archivo,$apertura);
+            }
+            
+        }
         //modificar posición de array segun ID
         //llamar a función guardar por C/id del aray retornado por listar
         
--- Clase03/guardar.php
+++ Clase03/guardar.php
@@ -1,15 +1,19 @@
 <?php
+include_once './funciones.php';
 $obj = new stdClass();
 // echo($_GET['nombre']);
 // $obj['nom'] = $_GET['nombre'];
 $obj->nombre = $_GET['nombre'];;
 $obj->apellido = $_GET['apellido'];;
 $codificado = json_encode($obj);
+var_dump($obj);
 $archivo = "archivo.txt";
 $modo = "a";
-$ar = fopen($archivo,$modo);  
+// $ar = fopen($archivo,$modo);  
 // fwrite($ar,"[".$codificado."]".",");
-fwrite($ar,$codificado.PHP_EOL);
-fclose($ar);
-//
+// fwrite($ar,$codificado.PHP_EOL);
+// fclose($ar);
+// Funciones::Guardar($obj,$archivo,$modo);
+Funciones::ModificarxID(2,$obj,$archivo);
+
 ?>
